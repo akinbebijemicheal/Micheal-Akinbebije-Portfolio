1382be30894dcdb4ebd0a574112bdca57b5825ab
Akinbebije Micheal <michealakinbebije@gmail.com>
2023-03-17 19:04:50 +0100

    first commit

---
+++ src/components/ContactUs.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Feel free to contact me for any work or suggestions below
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget">
+                <h4>
+                  <a href={resumeData.socialLinks[0].url} target="_blank"><i className='fa fa-linkedin'></i>   Linkedin {resumeData.linkedinId}</a>
+                </h4>
+                <h4>
+                  <a href={resumeData.socialLinks[4].url} target="_blank" ><i className='fa fa-envelope'></i>   Click here to mail me at michealakinbebije@gmail.com</a>
+                </h4>
+                <h4>
+                  <a href={resumeData.resumeLink} target="_blank"><i className='fa fa-file'></i>   View or download my resume here</a>
+                </h4>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,182 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Micheal Newton Akinbebije",
+    "role": "Full-Stack Developer (Backend Heavy) and Data Scientist",
+    "linkedinId":"Akinbebije Micheal",
+    "skypeid": "Your skypeid",
+    "roleDescription": "A Software Dveloper and Data Scientist that loves building scalable applications while deriving insights from complex datasets to make data-driven decisions. My expertise is in both front-end and back-end development, as well as statistical analysis and machine learning. I love networking and building myself in my free time",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/akinbebije-micheal-68626522a",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Akinbebijemicheal",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://mobile.twitter.com/AkinbebijeN",
+          "className":"fa fa-twitter"
+        },
+        {
+          "name":"phone",
+          "url":"https://mobile.twitter.com/AkinbebijeN",
+          "className":"fa fa-phone"
+        },
+        {
+          "name":"email",
+          "url":"https://mailto:michealakinbebije@gmail.com",
+          "className":"fa fa-envelope"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The Federal University of Technology Akure my B.Tech from here. As a Full-stack Developer and Data Scientist, I am proficient in both front-end and back-end development, building scalable, efficient and user-friendly applications. At the same time, I can analyze and derive insights from complex datasets, making data-driven decisions that enhance the user experience.My technical skills include proficiency in languages such as Php, JavaScript, Python and SQL, as well as experience with frameworks like React, Node, laravel and Django. I am familiar with designing and implementing databases, building RESTful APIs, and using version control tools such as Git. As a data scientist, they have experience in statistical analysis, machine learning algorithms, and data visualization. They are familiar with tools such as Jupyter Notebook, Pandas, NumPy, and scikit-learn. They have experience in data cleaning, feature engineering, and model selection and evaluation.one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Nigeria",
+    "website":"https://github.com/Akinbebijemicheal",
+    "education":[
+      {
+        "UniversityName":"Federal University of Technology Akure",
+        "specialization":"Agricultural Extension and Communication Technology",
+        "MonthOfPassing":"",
+        "YearOfPassing":"Still here",
+        "Achievements":"Currently on a Second Class Upper"
+      },
+      {
+        "UniversityName":"Wetindey Code Academy",
+        "specialization":"Fullstack Software Development & Graphics Design",
+        "MonthOfPassing":"August",
+        "YearOfPassing":"2020",
+        "Achievements":"Learnt Web Software Development & Graphics Design using HTML, CSS, Javascript, PHP, Mysql, Photoshop & Coreldraw"
+      },
+      {
+        "UniversityName":"Sidehustle Internship",
+        "specialization":"Backend & Frontend Dev using Nodejs & Reactjs",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2022",
+        "Achievements":"Completed in 2 months"
+      },
+      {
+        "UniversityName":"Future Academy Africa",
+        "specialization":"Data Science",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2022",
+        "Achievements":"Completed in a month"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Cashbuddy Inc",
+        "specialization":"Backend Software Developer",
+        "MonthOfLeaving":"Oct",
+        "YearOfLeaving":"2022",
+        "Achievements":"A startup fintech, I accomplished integrating majority of the best financial APIs in Africa and built a scalable backend application using php and nodejs"
+      },
+      {
+        "CompanyName":"Pangolin Inc",
+        "specialization":"Team Lead and Backend Developer",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2022, 1+ year",
+        "Achievements":"A startup aimed at logistics & transportation of goods, I led the team and developed an efficient backend code and tracking system api of customer goods"
+      },
+      {
+        "CompanyName":"Greenmouse Technology",
+        "specialization":"FullStack Developer(Backend main) & Data Analyst",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2022",
+        "Achievements":"Built over 4 projects & analysed heavy amounts of data in just a month timeframe"
+      },
+      {
+        "CompanyName":"Wetindey Code Academy",
+        "specialization":"Fullstack Developer and Tutor",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"Still Present",
+        "Achievements":"Tutored over 80 students on web development , built and deployed over a dozen softwares for the company"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      
+      {
+        "skillname":"NodeJS"
+      },
+      {
+        "skillname":"PHP"
+      },
+      {
+        "skillname":"ReactJs"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"MySql"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"AWS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Microsoft-Excel"
+      },
+      {
+        "skillname":"PowerBI"
+      },
+      {
+        "skillname":"JQUERY"
+      },
+      {
+        "skillname":"Others (HTML5, CSS, Docker, Laravel, Netlify, Heroku)"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/coffee.jpg",
+        "url":"https://cashbuddy.ng"
+        
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/2.png",
+        "url":"https://cashbuddy.ng"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/1.png",
+        "url":"https://cashbuddy.ng"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/coffee.jpg",
+        "url":"https://cashbuddy.ng"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"He's a really good team lead and Software Developer",
+        "name":"Cashbuddy Inc"
+      },
+      {
+        "description":"A solid Backend Developer and Tutor",
+        "name":"Wetindey Code Academy"
+      },
+      {
+        "description":"A quality Fullstack Dev & Data Scientist/Analyst",
+        "name":"Pangolin Inc"
+      }
+    ],
+    "resumeLink" : "https://drive.google.com/file/d/1vQSZoIzgtVAKoNaiKdRUtf999vIIFxSz/view?usp=drivesdk"
+  }
+  
+  export default resumeData
